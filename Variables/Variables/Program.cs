4bf7f9f4b37d49f70c6e28f4f36a43595f48ee06
Siyabonga Dlamini <focusoncoding@gmail.com>
2020-11-30 14:56:46 +0200

    done with Variables on Type Conversion

--- Variables/Variables/Program.cs
+++ Variables/Variables/Program.cs
@@ -6,7 +6,28 @@ namespace Variables
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            byte byteNumber = 2;
+            int count = 10;
+            float totalPrice = 20.95f;
+            decimal Dnumber = 1200.0m;
+            char character = 'A';
+            string firstName = "Mosh";
+            bool isWorking = false;
+            Console.WriteLine("{0} {1} {2} {3} {4}", count, totalPrice, character, firstName, isWorking);
+            Console.WriteLine("Hello World! it has started");
+            
+            // let the compiler figur out whats the type is
+            var myName = "Siyabonga";
+
+            const float Pi = 3.14f; // this value cant be changed
+
+            // CTRL + CLICK    see more about type
+
+            // check for range
+            Console.WriteLine("{0} {1}", byte.MaxValue, byte.MinValue);
+            Console.WriteLine("{0} {1}", float.MaxValue, float.MinValue);
+
+            
         }
     }
 }
