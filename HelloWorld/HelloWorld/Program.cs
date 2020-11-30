f23e936c8520e2c4026ab7d7ca9ef9b9843ac365
Siyabonga Dlamini <focusoncoding@gmail.com>
2020-11-30 13:19:57 +0200

    test commit

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Im back baby whos going to cery the bots");
+            // 31:00 111 2222
+        }
+    }
+}
---
+++ Variable/Variable/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+
+namespace Variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'A';
+            string firstName = "Mosh";
+            bool isWorking = false;
+            Console.WriteLine( count, totalPrice, character, firstName, isWorking);
+
+        }
+    }
+}
