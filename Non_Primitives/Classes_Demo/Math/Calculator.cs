278ae749ce2985fb95f3bf6e3401eaca64238c5a
Siyabonga Dlamini <focusoncoding@gmail.com>
2020-12-02 09:43:50 +0200

    done with classes demo on to Arrays

---
+++ Non_Primitives/Classes_Demo/Math/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+// now because i moved this class to here i need to add the namespace 
+namespace Classes_Demo.Math
+{
+    // add public access modifia to make this availeble every where
+    public class Calculater
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ Non_Primitives/Classes_Demo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+// add the PersonClass Namespace
+using Classes_Demo.Math;
+
+namespace Classes_Demo
+{
+    public class Person0
+    {
+        public string FirstNane;
+        public string LastName;
+
+        public void Introduce()
+        {
+            Console.WriteLine("My Name is :" + FirstNane + " " + LastName);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Siya = new Person0();
+            // assign fields to the new object
+            Siya.FirstNane = "Siyabonga";
+            Siya.LastName = "Dlamini";
+            Siya.Introduce();
+
+            // use person class in the Calculater folder
+            Calculater calculater = new Calculater();
+            var result = calculater.Add(1, 2);
+            Console.WriteLine(result); // output the variables value 
+        }
+    }
+}
