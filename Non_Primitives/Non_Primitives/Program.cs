ac9c1451859f0709685e59090bafba86158578c4
Siyabonga Dlamini <focusoncoding@gmail.com>
2020-12-02 08:29:22 +0200

    start learning non premitives

---
+++ Non_Primitives/Non_Primitives/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Non_Primitives
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
