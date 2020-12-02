f0341e1616dced579fef842bc72006a29506ce70
Siyabonga Dlamini <focusoncoding@gmail.com>
2020-12-02 11:22:05 +0200

    done with arrays demo on to strings

--- Non_Primitives/Classes_Demo/Program.cs
+++ Non_Primitives/Classes_Demo/Program.cs
@@ -11,11 +11,8 @@ namespace Classes_Demo
 
         public void Introduce()
         {
-            Console.WriteLine("My Name is :" + FirstNane + " " + LastName);
+            Console.WriteLine("My Name is : " + FirstNane + " " + LastName);
         }
-    }
-    class Program
-    {
         static void Main(string[] args)
         {
             var Siya = new Person0();
@@ -28,6 +25,48 @@ namespace Classes_Demo
             Calculater calculater = new Calculater();
             var result = calculater.Add(1, 2);
             Console.WriteLine(result); // output the variables value 
+
+            Console.WriteLine("Arrays Demo ----------------------------------------------------------------------");
+
+            // Arrays Demo
+            var number = new int[4];
+            number[0] = 1;
+            Console.WriteLine(number[0]);
+            Console.WriteLine(number[1]); // emty indexies will be [0]
+            Console.WriteLine(number[2]); // course return type is init | numbers
+            Console.WriteLine(number[3]);
+
+            var flags = new bool[3];
+            flags[0] = true;
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[1]); // emty elements will be [false]
+            Console.WriteLine(flags[2]); // course return type is bool
+
+            var names = new string[3] { "Siyabonga", "Nosipho", "Nondalo" };
+
+            Console.WriteLine("String Demo -----------------------------------------------------------------------");
+
+            var firstName_ = "MR Dlamini";
+            var lastName_ = "Dlamini";
+
+            var fullName_ = firstName_ + " " + lastName_;
+            var myfullName_ = string.Format("My name is : {0} {1}", firstName_, lastName_);
+
+            var names_ = new string[3] { "Sipho", "Senzo", "Somandla" };
+            // combine the strings using a , | statc methos on the string class JOIN
+            var formattedNames = string.Join(",", names_);
+            Console.WriteLine(formattedNames);
+
+            var textOne = "Hi Siya\nlook into the following paths\nC:\\folder1\\folder2\nC:\\folder3\\folder4";
+            Console.WriteLine(textOne);
+            // make the above look better using vabatim string
+            var textTo = @"Hi Siya\nlook into the following paths
+                            C:\folder1\folder2
+                            C:\folder3\folder4";
+            Console.WriteLine(textTo);
         }
+    }
+    class Program
+    {
     }
 }
