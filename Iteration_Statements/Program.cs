f849c41a310f26920136e113f403a2fdd1502ae9
Siyabonga Dlamini <focusoncoding@gmail.com>
2020-12-04 10:58:37 +0200

    done with Random Class

---
+++ Iteration_Statements/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Iteration_Statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // For Loop
+
+            for (int i = 0; i <= 10; i++)
+            {
+                // even numbers up to 10
+                if (i%2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+
+            for (int i = 0; i >= 1; i--)
+            {
+                // even numbers dicending order
+                if (i%2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            // ForEach Loop clearner then a for loop used with ENUMERABLES like string
+            var name = "John Smith";
+
+            for (int i = 0; i < name.Length; i++)
+            {
+                Console.WriteLine(name[i]);
+            }
+
+            // same as above using foreah
+            foreach (var character in name)
+            {
+                Console.WriteLine(character);
+            }
+
+            // iterating over an array
+            var numbers = new int[] { 1, 2, 3, 4 };
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            // While loop best used when i dont know ahard ot time how maney times i may need to itera around something
+            var ii = 0;
+            while (ii <= 10)
+            {
+                // count from two to 10
+                if (ii % 2 == 0)
+                {
+                    Console.WriteLine(ii);
+                }
+                ii++;
+            }
+
+            // get user input
+            while (true)
+            {
+                Console.WriteLine("Type your username");
+                var input = Console.ReadLine();
+
+                // check if form is filled exit if the user fiss nothing
+                if (String.IsNullOrWhiteSpace(input)) break;
+
+                Console.WriteLine("@Echo: " + input);
+            }
+        }
+    }
+}
---
+++ RandomClass/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace RandomClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            for (int i = 0; i < 10; i++)
+                Console.WriteLine(random.Next(1, 10)); // generat numbers 1 to 10
+
+            // see what the string 'a' is as ASCII in bytes
+            Console.WriteLine((int)'a');
+
+            // Generat random string into password
+            var randomString = new Random();
+            for (int i = 0; i < 10; i++)
+                // cast to (charater)
+                Console.WriteLine((char)randomString.Next(97, 122)); // A => Z 97 => 122
+
+            // a more axpresive way to writethe above code
+            var moreReadeble = new Random();
+            for (int i = 0; i < 10; i++)
+                // use Console.write to make the out write in the same line
+                Console.WriteLine((char)('a' + moreReadeble.Next(0, 26))); // because there a 26 charaters in the english language
+
+            // create the above but store it in a array buffer
+            var randomBuffer = new Random();
+            // make 10 a constso ill change it in one place in the futur if i need to change it
+            const int passwordlength = 10;      
+            var buffer = new char[passwordlength];
+            
+            for (int i = 0; i < passwordlength; i++)
+                buffer[i] = ((char)randomBuffer.Next(97, 122));
+            // create a string biested on ths array
+            var password = new string(buffer);
+            Console.WriteLine(password);
+        }
+    }
+}
