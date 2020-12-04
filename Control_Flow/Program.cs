6fb14b8e1fc23698c17123b7918790692f3d6f34
Siyabonga Dlamini <focusoncoding@gmail.com>
2020-12-04 09:24:18 +0200

    done with control_flow on to iteration statements

--- Control_Flow/Program.cs
+++ Control_Flow/Program.cs
@@ -6,7 +6,38 @@ namespace Control_Flow
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("let it flow..");
+
+            int hour = 10;
+
+            if (hour > 0 && hour < 12)
+            {
+                Console.WriteLine("Its morning.");
+            }
+            else if (hour >= 12 && hour < 18)
+            {
+                Console.WriteLine("Its afternoon");
+            }
+            else
+            {
+                Console.WriteLine("Its Morning");
+            }
+
+            bool isGoldCustomer = true;
+
+            float price;
+            if (isGoldCustomer)
+            {
+                price = 19.95f;
+            }
+            else
+            {
+                price = 29.95f;
+            }
+
+            // write the abouve as a condition
+            float price1 = (isGoldCustomer) ? 19.95f : 29.95f;
+            Console.WriteLine(price1);
         }
     }
 }
