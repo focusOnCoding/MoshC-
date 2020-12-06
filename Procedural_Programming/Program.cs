fd0ff18c5f92ef60105281d498ebfde86c024350
Siyabonga Dlamini <focusoncoding@gmail.com>
2020-12-06 03:59:10 +0200

    final grouping more readable and more reuserble

--- Procedural_Programming/Program.cs
+++ Procedural_Programming/Program.cs
@@ -6,7 +6,29 @@ namespace Procedural_Programming
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // putting code where it belong (Grouping code) (family) (familier)
+            // code for console and logic implementation should be separated
+
+            Console.WriteLine("What is your name: ");
+            var name = Console.ReadLine();
+            var reversed = ReverseName(name);
+            Console.WriteLine("Reversed name: " + reversed);
+        }
+        // logic group here leave the console above      
+        public static string ReverseName(string name)
+        {
+            var array = new char[name.Length];
+            for (int i = 0; i < name.Length; i++)
+            {
+                array[name.Length - i] = name[i - 1];
+            }
+
+            // cut this out
+            //var reversed = new string(array); 
+
+            // return the above strat away
+            //return reversed;
+            return new string(array);
         }
     }
 }
