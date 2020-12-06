1ee7ae6423f6a8ac67f3ce8a1efccfdaec18e26b
Siyabonga Dlamini <focusoncoding@gmail.com>
2020-12-06 03:43:27 +0200

    (commited in the write plae now) what my code looked like before i started grouping it

---
+++ Procedural_Programming/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Procedural_Programming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
